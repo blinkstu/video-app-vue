348580cda3145abc38dc1aa616808ee9394be445
Arys <arys@MacBook-Pro-user.local>
2019-08-30 11:40:03 +0600

    go

--- src/router.js
+++ src/router.js
@@ -1,3 +1,7 @@
+const nprogress = require("nprogress");
+import { Toast } from "vant";
+require("nprogress/nprogress.css");
+
 var routes = [
   {
     path: "/",
@@ -31,14 +35,14 @@ var routes = [
     component: () => import("./pages/Detail")
   },
   {
-    name: 'Login',
-    path: '/user/login',
+    name: "Login",
+    path: "/user/login",
     component: () => import("./pages/Login"),
     props: true
   },
   {
-    path: '/user/promotion',
-    component: () => import('./pages/Promotion')
+    path: "/user/promotion",
+    component: () => import("./pages/Promotion")
   },
   {
     path: "/user/register",
@@ -61,6 +65,21 @@ var routes = [
 import VueRouter from "vue-router";
 const router = new VueRouter({
   routes
+});
+
+var toastPageLoading;
+
+router.beforeEach((from, to, next) => {
+  toastPageLoading = Toast.loading({
+    duration: 0
+  });
+  next();
+});
+router.afterEach(() => {
+  if (toastPageLoading) {
+    toastPageLoading.clear();
+  }
+  window.scrollTo(0, 0);
 });
 
 export default router;
